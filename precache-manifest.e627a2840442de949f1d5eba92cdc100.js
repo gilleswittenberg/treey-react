4e5c3d073a9c5c3cdb711ba12ca97dc86d136c25
Gilles Wittenberg <mail@gilleswittenberg.com>
2020-05-02 21:03:46 +0200

    Updates

---
+++ precache-manifest.e627a2840442de949f1d5eba92cdc100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2838f1fbbfceae5e8eb8d2fa20f3195c",
+    "url": "/treey-react/index.html"
+  },
+  {
+    "revision": "d91bc9d3fa717cd97e82",
+    "url": "/treey-react/static/css/main.f57e500a.chunk.css"
+  },
+  {
+    "revision": "b150614893725874ea84",
+    "url": "/treey-react/static/js/2.461d685d.chunk.js"
+  },
+  {
+    "revision": "5509b05e5b9c43b321283d108cdc882f",
+    "url": "/treey-react/static/js/2.461d685d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91bc9d3fa717cd97e82",
+    "url": "/treey-react/static/js/main.c75a79f2.chunk.js"
+  },
+  {
+    "revision": "75f6cdb44a27c9701f60",
+    "url": "/treey-react/static/js/runtime-main.ca732ffc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.46229e42ffc15286b8449254b5692ba0.js"
+  "/treey-react/precache-manifest.e627a2840442de949f1d5eba92cdc100.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treey-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
